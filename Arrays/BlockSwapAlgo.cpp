f3bf3d407295aeabffc8522ba723fc725bbaa443
Amitesh-tiwari <113535850+Amitesh-tiwari@users.noreply.github.com>
2024-08-16 19:24:28 +0530

    changes done

--- Arrays/BlockSwapAlgo.cpp
+++ Arrays/BlockSwapAlgo.cpp
@@ -4,34 +4,22 @@ steps, where d is a positive integer.
 
 Note: Consider the array as circular.
 
-// Helper function to swap elements in the array
-void swap(int arr[], int fi, int si, int d);
-
-// Block Swap Algorithm
-void rotateArr(int arr[], int d, int n) {
-    if (d == 0 || d == n)
-        return;
-
-    if (n - d == d) {
-        swap(arr, 0, n - d, d);
-        return;
+class Solution{
+    public:
+    
+    //Function to rotate an array by d elements in counter-clockwise direction. 
+    void reverse(int arr[],int start,int end){
+        while(start<end){
+            swap(arr[start],arr[end]);
+            start++;
+            end--;
+        }
     }
-
-    if (d < n - d) {
-        swap(arr, 0, n - d, d);
-        rotateArr(arr, d, n - d);
-    } else {
-        swap(arr, 0, d, n - d);
-        rotateArr(arr + n - d, 2 * d - n, d);
+    void rotateArr(int arr[], int d, int n){
+        // code here
+        if(d>n) d=d%n;
+         reverse(arr,0,d-1);
+         reverse(arr,d,n-1);
+         reverse(arr,0,n-1);
     }
-}
-
-// Helper function to swap elements in the array
-void swap(int arr[], int fi, int si, int d) {
-    for (int i = 0; i < d; i++) {
-        int temp = arr[fi + i];
-        arr[fi + i] = arr[si + i];
-        arr[si + i] = temp;
-    }
-}
-}
+};
