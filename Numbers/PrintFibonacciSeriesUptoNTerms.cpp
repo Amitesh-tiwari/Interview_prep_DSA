104cdfbec9f76020a4668171353254b5eeee4bd9
Amitesh-tiwari <113535850+Amitesh-tiwari@users.noreply.github.com>
2024-08-31 00:45:03 +0530

    Fibonacci Upto N numbers

--- Numbers/PrintFibonacciSeriesUptoNTerms.cpp
+++ Numbers/PrintFibonacciSeriesUptoNTerms.cpp
@@ -17,3 +17,24 @@ int main() {
     cout<<current<<endl;
     return 0;
 }
+
+//approach
+int main(){
+    int n = 5;
+    if(n == 0){
+        cout<<0<<endl;
+    }
+     else if(n==1){
+        cout<<1<<endl;
+    }
+    else{
+        int fib[n+1];
+        fib[0] = 0;
+        fib[1] = 1;
+        for(int i=2;i<=n;i++){
+            fib[i] = fib[i-1] + fib[i-2];
+        }
+
+    }
+    
+}
