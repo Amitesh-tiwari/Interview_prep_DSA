b8f111b610e124b02acabf17341b0359630da706
Amitesh-tiwari <113535850+Amitesh-tiwari@users.noreply.github.com>
2024-09-03 15:28:16 +0530

    changes

--- d.cpp
+++ d.cpp
@@ -1,26 +1,35 @@
 Problem statement
-You have been given an array/list 'HEIGHTS' of length ‘N.
- 'HEIGHTS' represents the histogram and each element of 
- 'HEIGHTS' represents the height of the histogram bar. 
- Consider that the width of each histogram is 1.
+You are given two arrays 'A' and 'B' of size 'N' and 'M' respectively. Both these arrays are sorted in non-decreasing order. You have to find the intersection of these two arrays.
 
-You are supposed to return the area of the largest 
-rectangle possible in the given histogram.
+Intersection of two arrays is an array that consists of all the common elements occurring in both arrays.
 
-int largestRectangle(vector < int > & heights) {
-    int n = heights.size();
-    stack < int > s;
-    int maxArea = 0;
-    for (int i = 0; i <= n; i++) {
-        int h = (i == n ? 0 : heights[i]);
-        if (s.empty() || h >= heights[s.top()]) {
-            s.push(i);
-        } else {
-            int tp = s.top();
-            s.pop();
-            maxArea = max(maxArea, heights[tp] * (s.empty() ? i : i - 1 - s.top()));
-            i--;
+Note :
+1. The length of each array is greater than zero.
+2. Both the arrays are sorted in non-decreasing order.
+3. The output should be in the order of elements that occur in the original arrays.
+4. If there is no intersection present then return an empty array.
+
+#include <bits/stdc++.h> 
+vector<int> findArrayIntersection(vector<int> &arr1, int n, vector<int> &arr2, int m)
+{
+    vector<int> ans;
+    int i = 0, j = 0;
+    while (i < n && j < m)
+    {
+        if (arr1[i] == arr2[j])
+        {
+            ans.push_back(arr1[i]);
+            i++;
+            j++;
+        }
+        else if (arr1[i] < arr2[j])
+        {
+            i++;
+        }
+        else
+        {
+            j++;
         }
     }
-    return maxArea;
+    return ans;
 }
