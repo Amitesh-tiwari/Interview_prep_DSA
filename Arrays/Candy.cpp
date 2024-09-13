28f02fffdd5eba5e5b033261f32e3066b33a3f5e
Amitesh-tiwari <113535850+Amitesh-tiwari@users.noreply.github.com>
2024-09-13 20:15:05 +0530

    simple approach

--- Arrays/Candy.cpp
+++ Arrays/Candy.cpp
@@ -26,3 +26,31 @@ int candy(vector<int>& ratings){
     }
     return accumulate(candies.begin(), candies.end(), 0);
 }
+
+//simple 
+class Solution {
+public:
+    int candy(std::vector<int>& ratings) {
+        int n = ratings.size();
+        std::vector<int> candies(n, 1);
+
+        for (int i = 1; i < n; ++i) {
+            if (ratings[i] > ratings[i - 1]) {
+                candies[i] = candies[i - 1] + 1;
+            }
+        }
+
+        for (int i = n - 2; i >= 0; --i) {
+            if (ratings[i] > ratings[i + 1]) {
+                candies[i] = std::max(candies[i], candies[i + 1] + 1);
+            }
+        }
+
+        int totalCandies = 0;
+        for (int candy : candies) {
+            totalCandies += candy;
+        }
+
+        return totalCandies;
+    }
+};
