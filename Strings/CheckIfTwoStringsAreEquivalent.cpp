2bd7725c7ebed47d9e7bb26fe99c09b5382d6c0c
Amitesh-tiwari <113535850+Amitesh-tiwari@users.noreply.github.com>
2024-09-14 18:23:51 +0530

    Check if Two Strings are equivalent

---
+++ Strings/CheckIfTwoStringsAreEquivalent.cpp
@@ -0,0 +1,16 @@
+Given two string arrays word1 and word2, return true if 
+the two arrays represent the same string, and false 
+otherwise.
+
+A string is represented by an array if the array elements
+concatenated in order forms the string.
+
+//code
+bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2)
+
+{
+    string s1 = "", s2 = "";
+    for(auto x : word1) s1 += x;
+    for(auto x : word2) s2 += x;
+    return s1 == s2;
+}
