6353b02432e09f133f5c8e35084f09634f9075fc
Amitesh-tiwari <113535850+Amitesh-tiwari@users.noreply.github.com>
2024-10-03 00:05:32 +0530

    binary to octal

--- NumberSystem/BinaryTOOctal.cpp
+++ NumberSystem/BinaryTOOctal.cpp
@@ -1,7 +1,21 @@
 Convert Binary to Octal
 
+Problem Statement: Convert a binary number to an octal 
+number
 
-11
+//code
+#include <iostream>
+#include <cmath>
+using namespace std;
 
-0
-Problem Statement: Convert a binary number to an octal number
+int binaryToOctal(int n){
+    int ans = 0;
+    int x = 1;
+    while(n>0){
+        int y = n%1000;
+        ans += x*y;
+        x *= 10;
+        n /= 1000;
+    }
+    return ans;
+}
